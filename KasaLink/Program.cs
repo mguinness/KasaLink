adad71cfc42ab02f958513bab44ef91c2e34d650
mguinness <sufc@netzero.net>
2021-11-26 18:07:01 -0800

    Initial changes

---
+++ KasaLink/Program.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Buffers.Binary;
+using System.Collections.Generic;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text.Json;
+
+namespace KasaLink
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var cmd = new RootCommand
+            {
+                new Argument<string>("device", "Kasa device name."),
+                new Argument<string>("command", "Command to run."),
+                new Option(new[] { "--verbose", "-v" }, "Show device messages.")
+            };
+
+            cmd.Handler = CommandHandler.Create<string, string, bool>((device, command, verbose) =>
+            {
+                try
+                {
+                    var config = new ConfigurationBuilder()
+                      .AddIniFile("config.ini")
+                      .Build();
+
+                    var host = config["Devices:" + device];
+                    var cmnd = config["Commands:" + command];
+
+                    if (host == null || cmnd == null)
+                        throw new Exception("Config entry missing");
+
+                    if (verbose)
+                        Console.WriteLine(cmnd);
+                    var msg = SendCommand(host, cmnd);
+                    if (verbose)
+                        Console.WriteLine(msg);
+
+                    var root = JsonDocument.Parse(msg).RootElement;
+                    var first = root.EnumerateObject().First().Value;
+
+                    if (first.TryGetProperty("err_code", out var code))
+                        return code.GetInt32();
+                    else
+                        return first.EnumerateObject().First().Value.GetProperty("err_code").GetInt32();
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.WriteLine(ex.Message);
+                    return -1;
+                }
+            });
+
+            return cmd.Invoke(args);
+        }
+
+        public static string SendCommand(string host, string msg)
+        {
+            string result;
+
+            using (var client = new TcpClient())
+            {
+                client.Connect(host, 9999);
+
+                using (var stream = client.GetStream())
+                {
+                    var encrypted = Encrypt(msg);
+                    stream.Write(encrypted, 0, encrypted.Length);
+
+                    var lenBuffer = new byte[4];
+                    int bytesRead = stream.Read(lenBuffer, 0, lenBuffer.Length);
+                    var len = BinaryPrimitives.ReadInt32BigEndian(lenBuffer);
+
+                    var readBuffer = new byte[len];
+                    bytesRead = stream.Read(readBuffer, 0, readBuffer.Length);
+
+                    result = Decrypt(readBuffer);
+                }
+            }
+
+            return result;
+        }
+
+        private static byte[] Encrypt(string source)
+        {
+            var arr = new byte[4];
+            BinaryPrimitives.WriteInt32BigEndian(arr, source.Length);
+            var bytes = new List<byte>(arr);
+
+            int key = 171;
+            for (int i = 0; i < source.Length; i++)
+            {
+                var a = key ^ source[i];
+                key = a;
+                bytes.Add((byte)a);
+            }
+
+            return bytes.ToArray();
+        }
+
+        private static string Decrypt(byte[] source)
+        {
+            var str = String.Empty;
+
+            int key = 171;
+            for (int i = 0; i < source.Length; i++)
+            {
+                var a = key ^ source[i];
+                key = source[i];
+                str += (char)a;
+            }
+
+            return str;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# KasaLink
-Kasa smart device control from command line 
+ # Kasa Link
+[Kasa Smart](https://www.kasasmart.com/) is a subsidiary of [TP-Link](https://www.tp-link.com/) which manufactures smart home products like plugs and bulbs.  Several of these products use WiFi to allow remote control either locally or via the cloud.  This project is a console application that allows control of these devices from the command line and runs under Windows, OSX or Linux.
+
+## Introduction
+
+This project builds upon the work done by softScheck to reverse engineer the HS110 smart plug.  They discovered that among the open ports on the device is TCP port 9999 that accepts encrypted [JSON](https://en.wikipedia.org/wiki/JSON) to control the device.
+
+The encryption is a rudimentary [autokey cipher](https://en.wikipedia.org/wiki/Autokey_cipher) using XOR and a hardcoded starting key of 171.  Each request and response is preceded by four bytes in network byte order containing the payload length.
+
+## Configuration
+Before using the application you need to edit the `config.ini` file to define your devices and commands.  Add each device that you want to address along with the commands that are supported by each device. The key names can be changed to suit your own environment and are used as the parameters for the application from the command line.
+
+```INI
+[Devices]
+Plug=192.168.1.10
+Bulb=192.168.1.11
+
+[Commands]
+PlugOn={"system":{"set_relay_state":{"state":1}}}
+PlugOff={"system":{"set_relay_state":{"state":0}}}
+BulbOn={"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"on_off":1}}}
+BulbOff={"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"on_off":0}}}
+```
+
+## Usage
+The following details the parameters that can be passed to the application.
+
+```
+Usage:
+  KasaLink [options] <device> <command>
+
+Arguments:
+  <device>   Kasa device name.
+  <command>  Command to run.
+
+Options:
+  -v, --verbose   Show device messages.
+  --version       Show version information
+  -?, -h, --help  Show help and usage information
+```
+
+Based on the entries in the above example configuration file you can turn on the bulb using the following command.
+
+`KasaLink.exe bulb bulbon --verbose`
+
+The optional verbose argument emits both the request and response to the console for debugging purposes.
+
+Finally the application return code is set upon completion and each successful operation should return zero.
+
+`echo %errorlevel%`
+
+## Credits
+
+Reverse Engineering the TP-Link HS110
+https://www.softscheck.com/en/reverse-engineering-tp-link-hs110/
